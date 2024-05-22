5b950e8db7bddb7e608c4144800d7ab0f778e3aa
Ramya Ranabothu <ramya.r@deccansoft.net>
2024-05-22 15:22:05 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { GallaryComponent } from './gallary/gallary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'about', component:AboutComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'contactus', component:ContactusComponent},
+  {path: 'gallary', component:GallaryComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { GallaryComponent } from './gallary/gallary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactusComponent,
+    GallaryComponent
   ],
   imports: [
     BrowserModule,
